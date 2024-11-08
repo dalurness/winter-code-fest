fe339a158d732e8547ab8d2e8bb25568daa1a28a
Dallin Urness <dallin.urness96@gmail.com>
2024-11-08 00:47:28 -0700

    finish merging

--- src/components/DynamicSolution.tsx
+++ src/components/DynamicSolution.tsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from "react";
 
 // this component allows us to dynamically import components for question results that are unique to each specific question.
-export default ({ resultComponent }: { resultComponent: string }) => {
+export function DynamicSolution({ resultComponent }: { resultComponent: string }) {
   // btw...be aware of footguns when engaging in jank
   // See https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations for supported dynamic import formats
   const Component = lazy(() => import(/* @vite-ignore */`./solutions/${resultComponent}`));
